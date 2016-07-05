62db3f855cf5b038105b1649b8d01e46d36a96eb
Deniz Akkaya <denizakkaya@ddw.local>
2016-07-06 00:25:14 +0200

    fixing some cosmetics

--- index.php
+++ index.php
@@ -7,12 +7,22 @@
 	<h1>Reaction</h1>
 	<form action="eksisozluk.php" method="post">
 		<P>
-		URL: <input size="135" name="url" type="text" value="https://eksisozluk.com/denizde-sirtustu-yatmak--5143294">
+		URL: <input size="135" name="url" type="text" value="https://eksisozluk.com/28-haziran-2016-ataturk-havalimani-patlamasi--5139784">
 		<input type="submit" value="Send">
 		</P>
 	</form>
 	</div>
 
+	<div align="center" width="500px">
+		<h5><i>Please be patient while fetching and processing your request. Loading page may take couple of minutes. Therefore, you may need to change your php.ini settings.</i></h5>
+	</div>
+
+
+	<div align="center">
+		
+		<a href="https://github.com/denizea/reaction">Source Code</a> | 
+		<a href="https://github.com/denizea/reaction/blob/master/LICENSE">Licence</a>
+	</div>
 </body>
 
 </html>
